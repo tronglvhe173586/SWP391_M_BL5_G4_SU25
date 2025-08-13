c60e0c1bd24c3feef00793f31fcd7633c2f3f614
Lê Văn Trọng <tronglvhe173586@fpt.edu.vn>
2025-08-13 08:46:28 +0700

    create-project

---
+++ src/main/java/com/example/m_bl5_g4_su25/MBl5G4Su25Application.java
@@ -0,0 +1,13 @@
+package com.example.m_bl5_g4_su25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MBl5G4Su25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MBl5G4Su25Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/m_bl5_g4_su25/MBl5G4Su25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m_bl5_g4_su25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MBl5G4Su25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
