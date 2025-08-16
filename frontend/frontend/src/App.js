52e7b2e0db4e1c25e84460aef08d327b1c200de2
Maxim_Milian_Gnouc <97505268+maxim-lkc198@users.noreply.github.com>
2025-08-16 10:49:00 +0700

    feat: Class management frontend

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,18 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ResponsiveAppBar from "./components/ResponsiveAppBar.js";
-import UserManagement from "./pages/userManagement.js";
-import AddInstructor from "./pages/AddInstructor.js";
-import EditUser from "./pages/editUser.js";
-import Login from './pages/Login.js';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import UserManagement from './pages/userManagement';
+import AddInstructor from './pages/AddInstructor';
+import EditUser from './pages/editUser';
+// New imports for Class Management
+import ClassManagement from './pages/ClassManagement';
+import AddClass from './pages/AddClass';
+import ViewClass from './pages/ViewClass';
+import EditClass from './pages/EditClass';
+
 function App() {
   return (
     <Router>
       <ResponsiveAppBar />
       <Routes>
-        <Route path="/users" element={<UserManagement />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user-management" element={<UserManagement />} />
         <Route path="/add-instructor" element={<AddInstructor />} />
-        <Route path="/users/edit/:id" element={<EditUser />} />
-          <Route path="/login" element={<Login />} />
+        <Route path="/edit-user/:id" element={<EditUser />} />
+        {/* New routes for Class Management */}
+        <Route path="/class-management" element={<ClassManagement />} />
+        <Route path="/add-class" element={<AddClass />} />
+        <Route path="/view-class/:id" element={<ViewClass />} />
+        <Route path="/edit-class/:id" element={<EditClass />} />
       </Routes>
     </Router>
   );
