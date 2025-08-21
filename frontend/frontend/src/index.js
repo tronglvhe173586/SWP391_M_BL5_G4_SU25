be2f13d6b1b2a783f5d32db561faf1278bd5d1da
Lê Văn Trọng <tronglvhe173586@fpt.edu.vn>
2025-08-21 20:38:41 +0700

    refresh-token

--- frontend/frontend/src/index.js
+++ frontend/frontend/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { setupAxiosInterceptors } from './services/axiosClient';
 
+setupAxiosInterceptors();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
